ab27bd70ab7f12a2c794dfda2190b69371aa5e87
unknown <Ifemi18100@gmail.com>
2024-06-29 21:12:16 -1000

    Add Netlify configuration file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero/hero";
 import Destination from "./components/destination/Destination"
-import Search from './components/seartch/search'
+import Search from './components/search/search';
 import Selects from './components/selects/selects'
 import Imgcarousel from './components/carousel/imgcarousel'
 import Footer from './components/footer/footer'
